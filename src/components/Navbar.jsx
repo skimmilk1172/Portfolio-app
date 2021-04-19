9b5839b42d9f54bd5f94b962b5c511b9ec906f54
Sean Kim <sean.sihun.kim@gmail.com>
2021-04-19 15:47:55 +0000

    add more links to header and adjust some css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import './Navbar.scss';
 const Navbar = () => {
   return (
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container">
   <div className="container-fluid">
     <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='logo...'></img></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,15 +14,23 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">HOME</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">ABOUT ME</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">PROJECTS</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">CONTACT</a>
         </li>
 
       </ul>
 
     </div>
+  </div>
+
   </div>
 </nav>
   )
