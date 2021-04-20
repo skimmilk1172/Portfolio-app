ef7063ef14f6e09989c7e30cd59194a953adda94
Sean Kim <sean.sihun.kim@gmail.com>
2021-04-20 00:59:34 +0000

    add Header in components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className='header-wrapper'>
+      <div className='main-info'>
+        <h1>web development</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
