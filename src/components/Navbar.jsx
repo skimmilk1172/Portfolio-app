4a5cec3debdcecfe44fd0ce808753b1063705b4c
Sean Kim <sean.sihun.kim@gmail.com>
2021-04-20 00:47:10 +0000

    add fontawesome and use hamburger bar icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import logo from '../skim.png';
 import './Navbar.scss';
+// Reat fontawesome import
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 
 const Navbar = () => {
   return (
@@ -9,7 +13,7 @@ const Navbar = () => {
   <div className="container-fluid">
     <a className="navbar-brand" href="#"><img className='logo' src={logo} alt='logo...'></img></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+      <FontAwesomeIcon icon={faBars} style={{ color: 'black' }} />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ml-auto">
