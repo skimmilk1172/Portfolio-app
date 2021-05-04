e730acb5f21d3d0305156271f824627a552278fd
Sean Kim <sean.sihun.kim@gmail.com>
2021-05-04 20:40:55 +0000

    fixed css and add image in right placement

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -15,12 +15,14 @@ const AboutMe = () => {
       <container className='contain'>
         <div className='about-me-info'>
 
-        {/* <img src={Picture} /> */}
         <p data-aos='fade-right'>
           <h1>Who Am I?</h1>
           <h2>I am a full-stack developer eager to create web applications and exploring new technological trends. My love and curiosity towards logical thinking and creative freedom has led me to pursue web development. I am looking to join a diverse and functional team, working towards a common goal. Team oriented, but also very independently driven. </h2>
           <h2>Besides coding what I do during my free time, I like to play basketball, golf, tennis and snowboard. I also enjoy fishing and cooking.</h2>
         </p>
+          <div className='photo'>
+            <img data-aos='fade-left' src={Picture} />
+          </div>
       </div>
       </container> 
     </section>
